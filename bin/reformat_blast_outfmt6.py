abc081466bada7a0af592db86af1f2898969b8cb
DLBPointon <damonlbp@hotmail.co.uk>
2023-10-10 14:28:24 +0100

    Black Linting

--- bin/reformat_blast_outfmt6.py
+++ bin/reformat_blast_outfmt6.py
@@ -25,4 +25,4 @@ in_data = gpf.ll(in_path)
 for line in in_data:
     s = line.split()
     assert len(s) == 14
-    print('\t'.join( s[0:1] + s[4:] + s[2:3] ))
+    print("\t".join(s[0:1] + s[4:] + s[2:3]))
