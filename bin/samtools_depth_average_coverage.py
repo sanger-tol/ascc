a784236303164e8714d019bc036d18207c248213
yumisims <yy5@sanger.ac.uk>
2023-11-02 21:40:16 +0000

    re-written samtools_depth_average_coverage.py

--- bin/samtools_depth_average_coverage.py
+++ bin/samtools_depth_average_coverage.py
@@ -1,28 +1,67 @@
 #!/usr/bin/env python3
 """
-Script for finding the average coverage of each scaffold in a SAMtools depth output file
-written by Eerik Aunin (ea10)
+Script for finding the average coverage of each scaffold in a SAMtools depth output file.
+originally by Eerik Aunin (ea10)
+re-wriiten by Yumi Sims (yy5)
 """
 
 import sys
+import argparse
 import general_purpose_functions as gpf
 
-in_path = sys.argv[1]
-in_data = gpf.ll(in_path)
-scaffs_dict = dict()
+# Define the version number
+__version__ = '1.0.0'
 
-for line in in_data:
-    split_line = line.split()
-    scaff_name = split_line[0]
-    coverage = int(split_line[2])
-    if scaff_name in scaffs_dict:
-        scaffs_dict[scaff_name]["coverage_sum"] += coverage
-        scaffs_dict[scaff_name]["scaff_len"] += 1
-    else:
-        scaffs_dict[scaff_name] = dict()
-        scaffs_dict[scaff_name]["coverage_sum"] = coverage
-        scaffs_dict[scaff_name]["scaff_len"] = 1
+def display_version():
+    """Display the script's version."""
+    print(f"samtools_depth_average_coverage: {__version__}")
 
-for scaff_name in scaffs_dict:
-    average_coverage = scaffs_dict[scaff_name]["coverage_sum"] / scaffs_dict[scaff_name]["scaff_len"]
-    print(scaff_name + "," + str(average_coverage))
+def process_data(in_data):
+    """Process the input data and calculate scaffold coverage."""
+    scaffs_dict = {}
+    
+    for line in in_data:
+        split_line = line.split()
+        scaff_name = split_line[0]
+        coverage = int(split_line[2])
+        
+        if scaff_name in scaffs_dict:
+            scaffs_dict[scaff_name]["coverage_sum"] += coverage
+            scaffs_dict[scaff_name]["scaff_len"] += 1
+        else:
+            scaffs_dict[scaff_name] = {
+                "coverage_sum": coverage,
+                "scaff_len": 1
+            }
+    
+    return scaffs_dict
+
+def calculate_average_coverage(scaffs_dict):
+    """Calculate average coverage for each scaffold."""
+    results = []
+    for scaff_name, data in scaffs_dict.items():
+        average_coverage = data["coverage_sum"] / data["scaff_len"]
+        results.append((scaff_name, average_coverage))
+    return results
+
+def main():
+    """Main function to run the script."""
+    parser = argparse.ArgumentParser(description="Calculate average coverage for each scaffold.")
+    parser.add_argument('input_file', help='Path to the input SAMtools depth file')
+    parser.add_argument('--version', action='store_true', help='Display the version')
+
+    args = parser.parse_args()
+
+    if args.version:
+        display_version()
+        return
+
+    in_data = gpf.ll(args.input_file)
+    scaffs_dict = process_data(in_data)
+    results = calculate_average_coverage(scaffs_dict)
+
+    for scaff_name, average_coverage in results:
+        print(f"{scaff_name}, {average_coverage:.2f}")
+
+if __name__ == "__main__":
+    main()
