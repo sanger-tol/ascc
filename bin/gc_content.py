6f0c412d3df7ea9e5b226b04fb4e3cf915a2f014
yumisims <yy5@sanger.ac.uk>
2023-11-02 21:59:55 +0000

    amended gc_content.py to comprehension form

--- bin/gc_content.py
+++ bin/gc_content.py
@@ -2,8 +2,8 @@
 """
 Script for finding the GC content of each sequence in a multiFASTA file
 
-Written by Eerik Aunin @eeaunin
-
+Originally Written by Eerik Aunin @eeaunin
+Refactored by Yumi Sims @yumisims
 Adapted by Damon-Lee Pointon @DLBPointon
 """
 
@@ -13,16 +13,12 @@ import general_purpose_functions as gpf
 
 def main(fasta_path):
     fasta_data = gpf.read_fasta_in_chunks(fasta_path)
-    for header, seq in fasta_data:
-        header = header.split()[0]
-        seq = seq.upper()
-        gc_content = None
-        gc_count = seq.count("G") + seq.count("C")
-        seq_len = len(seq)
-        if seq_len > 0:
-            gc_content = gc_count / seq_len
-        gc_content_string = "{:.6f}".format(gc_content)
-        print("{}\t{}".format(header, gc_content_string))
+    results = [
+        (header.split()[0], "{:.6f}".format((seq.upper().count("G") + seq.upper().count("C")) / max(1, len(seq))))
+        for header, seq in fasta_data
+    ]
+    for header, gc_content_string in results:
+        print(f"{header}\t{gc_content_string}")
 
 
 if __name__ == "__main__":
