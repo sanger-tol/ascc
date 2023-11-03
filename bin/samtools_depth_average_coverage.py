51f14a805192ed8dbdf2f1644ead0fc56aca9a31
yumisims <yy5@sanger.ac.uk>
2023-11-03 21:07:07 +0000

    black

--- bin/samtools_depth_average_coverage.py
+++ bin/samtools_depth_average_coverage.py
@@ -10,25 +10,28 @@ import argparse
 import general_purpose_functions as gpf
 
 # Define the version number
-__version__ = '1.0.0'
+__version__ = "1.0.0"
+
 
 def display_version():
     """Display the script's version."""
     print(f"{__version__}")
 
+
 def process_data(in_data):
     """Process the input data and calculate scaffold coverage."""
     scaffs_dict = {}
-    
+
     for line in in_data:
         split_line = line.split()
         scaff_name, coverage = split_line[0], int(split_line[2])
-        
+
         scaffs_dict[scaff_name] = {
             "coverage_sum": scaffs_dict.get(scaff_name, {}).get("coverage_sum", 0) + coverage,
-            "scaff_len": scaffs_dict.get(scaff_name, {}).get("scaff_len", 0) + 1
+            "scaff_len": scaffs_dict.get(scaff_name, {}).get("scaff_len", 0) + 1,
         }
     return scaffs_dict
+
 
 def calculate_average_coverage(scaffs_dict):
     """Calculate average coverage for each scaffold."""
@@ -38,11 +41,12 @@ def calculate_average_coverage(scaffs_dict):
         results.append((scaff_name, average_coverage))
     return results
 
+
 def main():
     """Main function to run the script."""
     parser = argparse.ArgumentParser(description="Calculate average coverage for each scaffold.")
-    parser.add_argument('input_file', nargs='?', help='Path to the input SAMtools depth file')
-    parser.add_argument('--version', action='store_true', help='Display the version')
+    parser.add_argument("input_file", nargs="?", help="Path to the input SAMtools depth file")
+    parser.add_argument("--version", action="store_true", help="Display the version")
 
     args = parser.parse_args()
 
@@ -61,6 +65,7 @@ def main():
 
     for scaff_name, average_coverage in results:
         print(f"{scaff_name}, {average_coverage:.2f}")
+
 
 if __name__ == "__main__":
     main()
