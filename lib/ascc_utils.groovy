b68ce255eddfe390f59dcb3a537544cd5a1272ff
DLBPointon <damonlbp@hotmail.co.uk>
2025-12-01 20:44:59 +0000

    Add description

--- lib/ascc_utils.groovy
+++ lib/ascc_utils.groovy
@@ -1,4 +1,6 @@
 def getEmptyPlaceholder(idx) {
+    // index (idx) as input
+    // create an empty placeholder file with index in name
     def placeholder = file("${workDir}/EMPTY_PLACEHOLDER_${idx}")
     if (!placeholder.exists()) {
         placeholder.text = ''
